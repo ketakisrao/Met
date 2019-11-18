f7757e485313c5da273ca56f761087c456b94140
Wenqian <huwenqian@huwenqians-MBP.wv.cc.cmu.edu>
2019-11-18 14:35:08 -0500

    annimation

---
+++ scripts/script.js
@@ -0,0 +1,3 @@
+ScrollReveal().reveal('.met-intro'), { delay: 500,reset: true };
+ScrollReveal().reveal('.met-map', { delay: 500,reset: true });
+ScrollReveal().reveal('.timeline-1', { delay: 500,reset: true });
