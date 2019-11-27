6c70f4aad76841b747c00c930f79683032a51751
Caitlin Coyiuto <coyiutoc@gmail.com>
2019-11-26 22:00:57 -0500

    Flipped img timeline.

--- scripts/timeline/timeline-bar-generator.js
+++ scripts/timeline/timeline-bar-generator.js
@@ -2,18 +2,17 @@ function generateBarTimeline(data) {
 
   // Margin conventions
   var margin = {top: 0, right: 30, bottom: 30, left: 30};
-  var constWidth = d3.select(".bar-graph").node().getBoundingClientRect().width;
-  var constHeight = d3.select(".bar-graph").node().getBoundingClientRect().height;
+  var constWidth = d3.select(".bar-graph").node().getBoundingClientRect().width*0.8;
+  var constHeight = d3.select(".bar-graph").node().getBoundingClientRect().height*0.8;
   var width = constWidth - margin.left - margin.right,
-      height = width*0.2 - margin.top - margin.bottom;
+      height = constHeight*0.5 - margin.top - margin.bottom;
 
   var strokeWidth = 10;
 
   // Appends the svg to the chart-container div
   var svg = d3.select("#bar-svg")
               .append("svg")
-              .attr("viewBox", `0 0 ${width+margin.left+margin.right} ${height+margin.top+margin.bottom}`)
-              .attr("transform", "translate(0," + (constHeight - height*1.2) +")")
+              .attr("viewBox", `0 0 ${width+margin.left+margin.right} ${constHeight+margin.top+margin.bottom}`)
               .append("g")
               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -26,18 +25,27 @@ function generateBarTimeline(data) {
     .range([height, 0]);
 
   // Defines the x axis styles
-  var xAxis = d3.axisBottom()
+  var xAxis = d3.axisTop()
                 .scale(xScale)
                 .tickPadding(10)
-                // .ticks(11)
-                .tickFormat(function(d) { return d * 1})
+                .ticks(11)
+                .tickFormat(function(d) { return d * 1});
 
   ready(data);
 
   function ready(data) {
 
+    let max = d3.max(data, function(d) {
+      return d["Object End Date"];
+    })
+
+    let min = d3.min(data, function(d) {
+      return d["Object End Date"];
+    })
+
     // Defines the xScale max
-    xScale.domain(d3.extent(data, function(d) { return d["Object End Date"]; }));
+    //xScale.domain(d3.extent(data, function(d) { return d["Object End Date"]; }));
+    xScale.domain([min-2, max]);
 
     // Defines the yScale max
     yScale.domain([0, 100]);
@@ -46,12 +54,12 @@ function generateBarTimeline(data) {
     var xAxisGroup = svg.append("g")
                         .attr("class", "x axis")
                         .attr("transform", function() {
-                          return "translate(0," + height + ")";
+                          return "translate(0," + height*0.3 + ")";
                         })
                         .call(xAxis);
 
     var yearCounter = {};
-
+    var imgSize = 5;                   
     var images = svg.selectAll("images")
                     .data(data)
                     .enter()
@@ -66,16 +74,15 @@ function generateBarTimeline(data) {
                         yearCounter[d["Object End Date"]] = 0;
                       }
 
-                      let imgHeight = 5;
                       let multiple = yearCounter[d["Object End Date"]];
 
-                      let y = height*0.8 - (imgHeight * multiple) + 10;
+                      let y = height*0.3 + (imgSize * multiple) + 10;
 
                       return `translate(${x-10},${y})`;
                     })
                     .append('svg')
-                    .attr("width", 5)
-                    .attr("height", 5)
+                    .attr("width", imgSize)
+                    .attr("height", imgSize)
                     .append('image')
                     .attr('xlink:href', function(d) {
                       return d["Image URL"]
@@ -84,49 +91,42 @@ function generateBarTimeline(data) {
                     .attr("y", 0)
                     .attr("width", "100%")
                     .attr("height", "100%")
-                    .append('rect')
-                    .attr("x", 0)
-                    .attr("y", 0)
-                    .attr("width", 20)
-                    .attr("height", 20)
-                    .style("stroke", "white")
-                    .style("stroke-width", 2)
-
-                    
-
                   
-    // .on("mouseout", function(d) {
-    //   d3.select(this)
-    //     .transition().duration(100)
-    //     .attr("y1", 50)
-    //     .style("stroke-width", strokeWidth)
-    //     .style("stroke", color)
-    //     .style("opacity", 0.4);
+    images.on("mouseover", function(d) {
+            let artTitle = d["Title"] ? d["Title"] : "Untitled";
+            let maxChar = 40;
+            if (artTitle.length > maxChar) {
+              artTitle = artTitle.slice(0, maxChar) + "...";
+            }
 
-    //   tooltip.transition(300)
-    //     .style("opacity", 0)
-    // }) 
+            d3.select(".art-title")
+              .html(artTitle)
 
-    // Binds data to strips
-    // var drawstrips = svg.selectAll("line.percent")
-    //                     .data(data)
-    //                     .enter()
-    //                     .append("line")
-    //                     .attr("class", "percentline")
-    //                     .attr("x1", function(d,i) { return xScale(d["Object End Date"]); })
-    //                     .attr("x2", function(d) { return xScale(d["Object End Date"]); })
-    //                     // .attr("y1", 50)
-    //                     // .attr("y2", 100)
-    //                     .attr("y1", function() {
-    //                       return 0;
-    //                     })
-    //                     .attr("y2", function() {
-    //                       return 50;
-    //                     })
-    //                     .style("stroke", "blue")
-    //                     .style("stroke-width", strokeWidth)
-    //                     .style("opacity", 0.4)
+            d3.select(".art-type")
+              .html("TYPE: " + d["Object Name"])
 
-    
+            d3.select(".art-medium")
+              .html("MEDIUM: " + d["Medium"])
+
+            d3.select(".art-end-date")
+              .html("DATE: " + d["Object End Date"])
+            
+            d3.select("#bar-img")
+              .attr("src", d["Image URL"])
+
+            d3.select(".bar-column")
+              .style("display", "block");
+            
+            d3.select("#bar-img")
+              .style("display", "block");
+
+          })
+          .on("mouseout", function(d) {
+            d3.select(".bar-column")
+              .style("display", "none");
+
+            d3.select("#bar-img")
+              .style("display", "none");
+          })   
   }
 }
