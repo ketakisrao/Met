a150b29c0f7a18f6b535b671609517e5515938ae
ketakisrao <ketaki_8@hotmail.com>
2019-12-02 03:12:56 -0500

    Active selected artist

--- scripts/timeline/ajax-timeline.js
+++ scripts/timeline/ajax-timeline.js
@@ -1,44 +1,54 @@
-$(document).on('click','#degas-timeline-button',function(){
+$(document).on('click', '#degas-timeline-button', function () {
     renderBarTimeline("degas");
+    $('.bar-artist-icon').removeClass("active");
+    $(this).addClass("active");
 });
 
-$(document).on('click','#klimt-timeline-button',function(){
-  renderBarTimeline("klimt");
+$(document).on('click', '#klimt-timeline-button', function () {
+    renderBarTimeline("klimt");
+    $('.bar-artist-icon').removeClass("active");
+    $(this).addClass("active");
 });
 
-$(document).on('click','#hokusai-timeline-button',function(){
-  renderBarTimeline("hokusai");
+$(document).on('click', '#hokusai-timeline-button', function () {
+    renderBarTimeline("hokusai");
+    $('.bar-artist-icon').removeClass("active");
+    $(this).addClass("active");
 });
 
-$(document).on('click','#rodin-timeline-button',function(){
-  renderBarTimeline("rodin");
+$(document).on('click', '#rodin-timeline-button', function () {
+    renderBarTimeline("rodin");
+    $('.bar-artist-icon').removeClass("active");
+    $(this).addClass("active");
 });
 
-$(document).on('click','#pencz-timeline-button',function(){
-  renderBarTimeline("pencz");
+$(document).on('click', '#pencz-timeline-button', function () {
+    renderBarTimeline("pencz");
+    $('.bar-artist-icon').removeClass("active");
+    $(this).addClass("active");
 });
 
 function renderBarTimeline(id) {
 
-  // !!! Note CORS enabled for localhost
-  let address = "https://met-server-nyc.herokuapp.com/timeline?artist=" + id;
+    // !!! Note CORS enabled for localhost
+    let address = "https://met-server-nyc.herokuapp.com/timeline?artist=" + id;
 
-  var data = $.ajax({
-    url: address,
-    type: 'GET',
-    cache: false
-  });
+    var data = $.ajax({
+        url: address,
+        type: 'GET',
+        cache: false
+    });
 
-  $.when(data).then(function (dataResp) {
-      let dataInput = dataResp.results;
-      generateBarTimeline(dataInput);
-      
+    $.when(data).then(function (dataResp) {
+        let dataInput = dataResp.results;
+        generateBarTimeline(dataInput);
+
     }, function (jqXHR, textStatus, errorThrown) {
         var x1 = data;
         if (x1.readyState != 4) {
             x1.abort();
         }
         alert('GET REQUESTS FAILED');
-  });
+    });
 
 }
